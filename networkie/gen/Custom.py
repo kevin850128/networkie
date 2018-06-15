1761f1f9acc470307d5509994b6e725ae6598764
Kevin Jiang <kevin040208@gmail.com>
2018-06-16 01:25:34 +0800

    submit hw3

--- networkie/gen/Custom.py
+++ networkie/gen/Custom.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+import pandas as pd
 
 class LoadFromFile(object):
     def __init__(self):
@@ -7,9 +7,8 @@ class LoadFromFile(object):
         Initiate variables for the class.
         """
         self.g = nx.Graph()
-
+        
         pass
-
     def from_edgelist(self, path):
         '''
         Read graph in edgelist txt format from `path`.
@@ -36,8 +35,26 @@ class LoadFromFile(object):
         print('Edgelist txt data successfully loaded into a networkx Graph!')
         return self.g
 
-    def from_in_class_network(self):  # This is Prob. 3-a.
+    def from_in_class_network(self, path):  # This is Prob. 3-a.
         '''
-        Write your code documentation here.  # This is Prob. 4-a.
+        First, create a dictionary that store every id's connected node numbers.
+        Then use the dictionary with for loop to create the edge list for our network.
+        Finally, we can put nodes and edges into our empty network. 
         '''
+        rawdata = pd.read_csv(path, delimiter = "\t")
+        md={}
+        for i in rawdata.index:
+            if rawdata.loc[i,"IDs-of-acquaintances"]==" ":
+                continue
+            elif type(eval(rawdata.loc[i,"IDs-of-acquaintances"]))==tuple:
+                md[rawdata.loc[i,"ID"]] = list(eval(rawdata.loc[i,"IDs-of-acquaintances"]))
+            else:
+                md[rawdata.loc[i,"ID"]] = [eval(rawdata.loc[i,"IDs-of-acquaintances"])]
+        edge = []
+        for d in md:
+            for k in range(len(md[d])):
+                edge.append((d,md[d][k]))
+        node = list(rawdata["ID"])
+        self.g.add_nodes_from(node)
+        self.g.add_edges_from(edge)
         return self.g
